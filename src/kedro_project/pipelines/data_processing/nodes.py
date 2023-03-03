c19392da1fe3e4800018b0fc05145489e5f20fab
o002629 <gabriel.palomares.ruiz@gmail.com>
2023-03-03 20:47:36 +0100

    “avanzandoconkedro2”

---
+++ src/kedro_project/pipelines/data_processing/nodes.py
@@ -0,0 +1,85 @@
+"""
+This is a boilerplate pipeline 'data_processing'
+generated using Kedro 0.18.5
+"""
+import pandas as pd
+"""Es una función interna por tener barra baja
+YA AQUÍ DEVOLVEMOS TIPOS DE LAS VARIABLES Y LO QUE DEVUELVE """
+def _is_true(x: pd.Series,) -> pd.Series:
+
+  return x == "t"
+"""DEVUELVE BOOLEANO SI X=='t'. ES LA TÍPICA FUNCIÓN PARA CONVERTIR UNA CADENA 'T' EN TRUE, QUE ES COMO DEBE TRABAJARSE"""
+
+"""Es una función interna por tener barra baja"""
+def _parse_percentage(x: pd.Series) -> pd.Series:
+
+  x = x.str.replace("%", "")
+
+  x = x.astype(float) / 100
+
+  return x
+  """CAMBIA UN EJEMPLO: 10% POR 0.1, QUE ES MÁS ÚTIL PARA OPERAR.. NO NOS VALE PARA NADA 10%"""
+"""Es una función interna por tener barra baja"""
+def _parse_money(x: pd.Series) -> pd.Series:
+
+  x = x.str.replace("$", "").str.replace(",", "")
+
+  x = x.astype(float)
+
+  return x
+"""QUITA LOS DOLARES Y COMAS"""
+def preprocess_companies(companies: pd.DataFrame) -> pd.DataFrame:
+
+  """Preprocesses the data for companies.
+  
+  FIJAOS!!! CADA FUNCIÓN DE PYTHON, ES UN NODO EN EL PIPELINE!!!!!
+
+
+
+  Args:
+
+    companies: Raw data.
+
+  Returns:
+
+    Preprocessed data, with `company_rating` converted to a float and
+
+    `iata_approved` converted to boolean.
+
+  """
+
+  companies["iata_approved"] = _is_true(companies["iata_approved"])
+
+  companies["company_rating"] = _parse_percentage(companies["company_rating"])
+
+  return companies
+
+
+
+
+
+def preprocess_shuttles(shuttles: pd.DataFrame) -> pd.DataFrame:
+
+  """Preprocesses the data for shuttles.
+
+
+
+  Args:
+
+    shuttles: Raw data.
+
+  Returns:
+
+    Preprocessed data, with `price` converted to a float and `d_check_complete`,
+
+    `moon_clearance_complete` converted to boolean.
+
+  """
+
+  shuttles["d_check_complete"] = _is_true(shuttles["d_check_complete"])
+
+  shuttles["moon_clearance_complete"] = _is_true(shuttles["moon_clearance_complete"])
+
+  shuttles["price"] = _parse_money(shuttles["price"])
+
+  return shuttles
---
+++ src/kedro_project/pipelines/data_processing/pipeline.py
@@ -0,0 +1,29 @@
+"""
+This is a boilerplate pipeline 'data_processing'
+generated using Kedro 0.18.5
+"""
+
+from kedro.pipeline import Pipeline, node, pipeline
+
+from .nodes import preprocess_companies, preprocess_shuttles
+"""aqui importamos las funciones de nodes (archivo nodes de la carpeta que tiene pipeline)"""
+
+"""Ahora creamos un pipeline con los nodos"""
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline(
+        [
+            node(
+               
+                func=preprocess_companies,
+                inputs="companies",
+                outputs="preprocessed_companies",
+                name="preprocess_companies_node",
+            ),
+            node(
+                func=preprocess_shuttles,
+                inputs="shuttles",
+                outputs="preprocessed_shuttles",
+                name="preprocess_shuttles_node",
+            ),
+        ]
+    )
