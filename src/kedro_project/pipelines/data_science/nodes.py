03afc02a597323597fbe37c2d08d613f1d780832
gpalomares2022 <gabriel.palomares.ruiz@gmail.com>
2023-03-09 11:05:31 +0100

    9marzo ya ok

--- src/kedro_project/pipelines/data_science/nodes.py
+++ src/kedro_project/pipelines/data_science/nodes.py
@@ -32,7 +32,15 @@ def split_data(data: pd.DataFrame, parameters: Dict) -> Tuple:
 
 def train_model(
       X_train: pd.DataFrame, y_train: pd.DataFrame, parameters: Dict) -> RandomForestRegressor:
-     rf = RandomForestRegressor(parameters["max_depth"])
+     
+     max_depth= parameters["max_depth"]
+     n_estimators= parameters["n_estimators"]
+     learning_rate= parameters["learning_rate"]
+     params_lgbm={'max_depth':max_depth, 'n_estimators': n_estimators, 'learning_rate': learning_rate}
+     params_random_forest={'max_depth':max_depth}
+     rf= RandomForestRegressor (**params_random_forest)   
+     #rf = LGBMRegressor(**params_lgbm)
+
      rf.fit(X_train, y_train)
      return rf
 
@@ -51,4 +59,5 @@ def evaluate_model(
     y_pred = regressor.predict(X_test)
     score = r2_score(y_test, y_pred)  #Correlacion
     logger.info("Model has a coefficient R^2 of %.3f on test data.", score)
-    logger.info("With parameter %.f max_depth.", parameters["max_depth"])
+    #logger.info("With LGBMRegressor and parameters %.f max_depth, %.f n_estimators, %.3f learning_rate", parameters["max_depth"], parameters["n_estimators"],parameters["learning_rate"] )
+    logger.info("With RandomForest and parameters %.f max_depth", parameters["max_depth"])
