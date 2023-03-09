2e320932d319cef523004e368874e9466b21575f
gpalomares2022 <gabriel.palomares.ruiz@gmail.com>
2023-03-09 10:14:43 +0100

    modified:   conf/base/catalog.yml 	new file:   conf/base/parameters/data_science.yml 	deleted:    notebooks/Notebookejemplokedro.ipynb 	new file:   notebooks/Notebookejemplokedro_conloadprofe.ipynb 	new file:   notebooks/Notebookejemplokedro_sin_loadkedro.ipynb 	modified:   src/kedro_project/pipelines/data_processing/nodes.py 	modified:   src/kedro_project/pipelines/data_processing/pipeline.py 	new file:   src/kedro_project/pipelines/data_science/README.md 	new file:   src/kedro_project/pipelines/data_science/__init__.py 	new file:   src/kedro_project/pipelines/data_science/pipeline.py 	modified:   src/requirements.txt 	new file:   src/tests/pipelines/data_science/__init__.py 	new file:   src/tests/pipelines/data_science/test_pipeline.py

--- src/kedro_project/pipelines/data_processing/nodes.py
+++ src/kedro_project/pipelines/data_processing/nodes.py
@@ -13,7 +13,7 @@ def _is_true(x: pd.Series,) -> pd.Series:
 """Es una función interna por tener barra baja"""
 def _parse_percentage(x: pd.Series) -> pd.Series:
 
-  x = x.str.replace("%", "")
+  x = x.astype(str).str.replace("%", "")
 
   x = x.astype(float) / 100
 
@@ -22,7 +22,7 @@ def _parse_percentage(x: pd.Series) -> pd.Series:
 """Es una función interna por tener barra baja"""
 def _parse_money(x: pd.Series) -> pd.Series:
 
-  x = x.str.replace("$", "").str.replace(",", "")
+  x = x.astype(str).str.replace("$", "").str.replace(",", "")
 
   x = x.astype(float)
 
@@ -83,3 +83,42 @@ def preprocess_shuttles(shuttles: pd.DataFrame) -> pd.DataFrame:
   shuttles["price"] = _parse_money(shuttles["price"])
 
   return shuttles
+
+def create_model_input_table(
+
+        shuttles: pd.DataFrame,
+
+        companies: pd.DataFrame,
+
+        reviews: pd.DataFrame,
+
+        ) -> pd.DataFrame:
+
+    
+
+    rated_shuttles = shuttles.merge(
+
+        reviews,
+
+        left_on = 'id',
+
+        right_on ='shuttle_id',
+
+    )
+
+
+
+    table = rated_shuttles.merge(
+
+        companies,
+
+        left_on = 'company_id',
+
+        right_on = 'id',
+
+        ).dropna()
+
+    
+
+    return table
+
--- src/kedro_project/pipelines/data_processing/pipeline.py
+++ src/kedro_project/pipelines/data_processing/pipeline.py
@@ -5,7 +5,7 @@ generated using Kedro 0.18.5
 
 from kedro.pipeline import Pipeline, node, pipeline
 
-from .nodes import preprocess_companies, preprocess_shuttles
+from .nodes import preprocess_companies, preprocess_shuttles, create_model_input_table
 """aqui importamos las funciones de nodes (archivo nodes de la carpeta que tiene pipeline)"""
 
 """Ahora creamos un pipeline con los nodos que queramos eah"""
@@ -25,5 +25,17 @@ def create_pipeline(**kwargs) -> Pipeline:
                 outputs="preprocessed_shuttles",
                 name="preprocess_shuttles_node",
             ),
+            node(
+
+                func=create_model_input_table,
+
+                inputs=["preprocessed_shuttles","preprocessed_companies", "reviews"],
+
+                outputs="model_input_table",
+
+                name="create_model_input_table_node",
+
+            ),
         ]
     )
+"""Se puede usar la misma función en nodos diferentes con imputs diferentes. Típica función genérica que puedes preparar para varios"""
