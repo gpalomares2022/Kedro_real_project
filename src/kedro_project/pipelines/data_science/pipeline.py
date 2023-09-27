891ad90190fb4d89ac2347b21e2ee3e3139c6d10
gpalomares2022 <gabriel.palomares.ruiz@gmail.com>
2023-09-27 15:52:11 +0200

    Solución de matplotlib ante problema de profiling

--- src/kedro_project/pipelines/data_science/pipeline.py
+++ src/kedro_project/pipelines/data_science/pipeline.py
@@ -7,12 +7,7 @@ from .nodes import recommendation_collaborative_filtering_user_based
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
-            #node(
-             #   func=predict_collaborative_filtering_ser_based,
-              #  inputs=["data_matrix", "params:model_options","csv_sintomas", "csv_enfermedades", "clean_and_processed_enfermedades"],
-               # outputs="data_calculated_scoring",
-                #name="predict_collaborative_filtering_ser_based_node",
-            #)
+          
            
 
             
